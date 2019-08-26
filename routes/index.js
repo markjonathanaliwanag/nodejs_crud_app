09ec6c229a41fe87863e5b70fb78fbbdc6386f94
Mac Aliwanag <markjonathanaliwanag@gmail.com>
2019-08-26 17:39:31 +0800

    Delete item done

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ app.get('/', function(req, res){
     res.render('index', {title: 'NodeJS CRUD App'})
 })
 
+// app.get('/items/add', function(req, res){
+//     res.render('items/add', {title: 'NodeJS CRUD App'})
+// })
+
 module.exports = app
--- routes/items.js
+++ routes/items.js
@@ -11,7 +11,8 @@ var pool = mysql.createPool({
     multipleStatements: true
 })
 
-app.get('/', function(req, res, next){
+// GET ITEMS FROM DB
+app.get('/items', function(req, res, next){
     req.getConnection(function(error, conn){
         pool.query("SELECT * FROM items ORDER by id DESC", function(err, rows, fields){
             if(err){
@@ -25,6 +26,153 @@ app.get('/', function(req, res, next){
                     title: 'Item List',
                     data: rows
                 })
+            }
+        })
+    })
+})
+
+// INSERT ITEM TO DB
+app.post('/add', function(req, res, next){
+    req.assert('name', 'Name is required').notEmpty()
+    req.assert('qty', 'Quantity is required').notEmpty()
+    req.assert('amount', 'Name is required').notEmpty()
+
+    var errors = req.validationErrors()
+
+    if(!errors){
+        var item = {
+            name: req.sanitize('name').escape().trim(),
+            age: req.sanitize('qty').escape().trim(),
+            email: req.sanitize('amount').escape().trim()
+        }
+
+        req.getConnection(function(error, conn){
+            pool.query("INSERT INTO users SET ?", item, function(err, result){
+                if(err){
+                    req.flash('error', err)
+                    res.render('item/add', {
+                        title: 'Add New Item',
+                        name: item.name,
+                        qty: item.qty,
+                        amount: item.amount,
+                    })
+                } else {
+                    req.flash('success', 'Item added successfully!')
+                    res.render('item/add', {
+                        title: 'Add New Item',
+                        name: '',
+                        qty: '',
+                        amount:'',
+                    })
+                }
+            })
+        })
+    } else {
+        var error_msg = ''
+        errors.forEach(function(error){
+            error_msg += error_msg + '<br/>'
+        })
+
+        req.flash('error', error_msg)
+
+        res.render('item/add', {
+            title: 'Add New Item',
+            name: req.body.name,
+            qty: req.body.age,
+            amount: req.body.amount,
+        })
+    }
+})
+
+// GET AN ITEM FOR UPDATE
+app.get('/edit/(:id)', function(req, res, next){
+    req.getConnection(function(error, conn){
+        pool.query("SELECT * FROM items WHERE id = " + req.params.id, function(err, rows, fields){
+            if(err) throw err
+
+            if(rows.length <= 0){
+                req.flash('error', 'Item not found!')
+                res.redirect('/items')
+            } else {
+                res.render('item/edit', {
+                    title: 'Edit Item',
+                    id: rows[0].id,
+                    name: rows[1].name,
+                    qty: rows[2].qty,
+                    amount: rows[3].amount
+                })
+            }
+        })
+    })
+})
+
+// UPDATE AN ITEM
+app.put('/edit/(:id)', function(req, res, next){
+    req.assert('name', 'Name is required').notEmpty()
+    req.assert('qty', 'Quantity is required').notEmpty()
+    req.assert('amount', 'Name is required').notEmpty()
+
+    var errors = req.validationErrors()
+
+    if(!errors){
+        var item = {
+            name: req.sanitize('name').escape().trim(),
+            age: req.sanitize('qty').escape().trim(),
+            email: req.sanitize('amount').escape().trim()
+        }
+
+        req.getConnection(function(error, conn){
+            pool.query("UPDATE items SET ? WHERE id = " + req.params.id, item, function(err, result){
+                if(err){
+                    req.flash('error', err)
+                    res.render('item/edit', {
+                        title: 'Edit Item',
+                        id: req.params.id,
+                        name: req.params.name,    
+                        qty: req.params.qty,
+                        amount: req.params.amount,
+                    })
+                } else {
+                    req.flash('success', 'Item Updated Successfully!')
+                    res.render('item/edit', {
+                        title: 'Edit Item',
+                        id: req.params.id,
+                        name: req.params.name,    
+                        qty: req.params.qty,
+                        amount: req.params.amount,
+                    })
+                }
+            })
+        })
+    } else {
+        var error_msg = ''
+        errors.forEach(function(error){
+            error_msg += error_msg + '<br/>'
+        })
+
+        req.flash('error', error_msg)
+
+        res.render('item/edit', {
+            title: 'Edit Item',
+            name: req.body.name,
+            qty: req.body.age,
+            amount: req.body.amount,
+        })
+    }
+})
+
+// DELETE AN ITEM
+app.delete('/delete/(:id)', function(req, res, next){
+    var item = {id: req.params.id}
+
+    req.getConnection(function(error, conn){
+        pool.query("DELETE FROM items WHERE id = " + req.params.id, item, function(err, rows, fields){
+            if(err){
+                req.flash('error', err)
+                res.redirect('/items')
+            } else {
+                req.flash('success', 'Item Deleted!')
+                res.redirect('/items')
             }
         })
     })
